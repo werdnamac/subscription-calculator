646e723ecddc6da3b6176034270b1245f7a4a6be
andy <werdnamac@gmail.com>
2022-05-21 15:51:26 -0400

    initial commmit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// Your JS code here
+var subTypeElement = document.querySelector("#subscription");
+var SubDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1;
+
+subTypeElement.addEventListener("change", function (e) {
+  subType = e.target.value;
+  //console.log(subType);
+  return updateSubscriptionDiv();
+});
+
+SubDurationElement.addEventListener("change", function (e) {
+  subDuration = Number(e.target.value);
+  //console.log(subDuration);
+  return updateSubscriptionDiv();
+});
+
+var updateSubscriptionDiv = function () {
+  var monthlyCost = 5;
+  if (subType === "standard") {
+    monthlyCost = 7;
+  } else if (subType === "premium") {
+    monthlyCost = 10;
+  }
+  var total = subDuration * monthlyCost;
+  result.innerText = `You have chosen a ${subType} 
+  subscription for ${subDuration} months. 
+  Your total cost is $${total}`;
+};
